5f2cd994758dd1b8afeb0b27603c6b6b6e1f6d04
Marcos Benevides <marcos.schonfinkel@gmail.com>
2025-09-20 15:53:38 -0400

    org: improve presentation content (iii) (#6)
    * org: improve presentation content (iii)

--- benchmark.sh
+++ benchmark.sh
@@ -1,3 +1,5 @@
-#!/usr/bin/env bash
+#!/usr/bin/env nix
+#! nix shell nixpkgs#erlang --command bash
 
-nix shell nixpkgs#erlang --command sh -c "erlc benchmark.erl && erl -noshell -s benchmark start 1000000 -s init stop"
+erlc benchmark.erl
+erl -noshell -s benchmark start 1000000 -s init stop
--- flake.nix
+++ flake.nix
@@ -1,137 +1,110 @@
 {
   description = "Development Environment";
-
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
     devenv = {
       url = "github:cachix/devenv";
       inputs.nixpkgs.follows = "nixpkgs";
     };
+    flake-parts = {
+      url = "github:hercules-ci/flake-parts";
+    };
   };
 
-  outputs = inputs@{ self, devenv, nixpkgs, ... }:
-    let
-      # System types to support.
-      supportedSystems = [ "x86_64-linux" "x86_64-darwin" "aarch64-darwin" ];
-
-      # Helper function to generate an attrset '{ x86_64-linux = f "x86_64-linux"; ... }'.
-      forAllSystems = nixpkgs.lib.genAttrs supportedSystems;
-
-      # Nixpkgs instantiated for supported system types.
-      nixpkgsFor = forAllSystems (system: import nixpkgs {
-        inherit system;
-        config.allowUnfree = true;
-      });
-
-      mkPkgs = system: nixpkgsFor."${system}";
-
-      mkEnv = pkgs: pkgs.texlive.combine {
-        inherit (pkgs.texlive)
-          beamer
-          beamertheme-simpleplus
-          collection-basic
-          collection-fontsextra
-          collection-fontsrecommended
-          collection-langenglish
-          collection-langportuguese
-          collection-latex
-          collection-latexextra
-          collection-mathscience
-          enumitem
-          fancyhdr
-          graphics
-          graphics-cfg
-          graphviz
-          hyphen-portuguese
-          latexmk
-          textcase
-          scheme-medium
-        ;
-      };
-    in
-    {
-      # nix build
-      packages = forAllSystems (system:
+  outputs = inputs@{ self, devenv, flake-parts, nixpkgs, ... }:
+    flake-parts.lib.mkFlake { inherit inputs; } {
+      systems = [ 
+        "x86_64-linux"
+        "aarch64-linux"
+        "aarch64-darwin"
+        "x86_64-darwin"
+      ];
+      perSystem = { pkgs, system, ... }: 
         let
-          pkgs = mkPkgs system;
-          texenv = mkEnv pkgs;
-          customEmacs = (pkgs.emacsPackagesFor pkgs.emacs-nox).emacsWithPackages (
-            epkgs:
-            with epkgs.melpaPackages;
-            [
-              citeproc
-              htmlize
-            ]
-            ++ (with epkgs.elpaPackages; [
-              org
-            ])
-          );
-        in
-        {
-          # nix build
-          default = pkgs.stdenvNoCC.mkDerivation rec {
-            name = "presentation";
-            src = ./.;
-            buildInputs = with pkgs; [ bash coreutils customEmacs gnumake texenv ];
-            phases = ["unpackPhase" "buildPhase" "installPhase"];
-            buildPhase = ''
-              export PATH="${pkgs.lib.makeBinPath buildInputs}:$PATH"
-              export XDG_CACHE_HOME="$(mktemp -d)"
-              export HOME="$(mktemp -d)"
-              make build
-            '';
-            installPhase = ''
-              mkdir -p $out
-              cp presentation.pdf $out
-            '';
-          };
-        }
-      );
-
-      defaultPackage = forAllSystems (system: self.packages.${system}.slides);
-
-      devShells = forAllSystems (system:
-        let
-          pkgs = mkPkgs system;
-          texenv = mkEnv pkgs;
-          customEmacs = (pkgs.emacsPackagesFor pkgs.emacs-nox).emacsWithPackages (
-            epkgs:
-            with epkgs.melpaPackages;
-            [
-              citeproc
-              htmlize
-            ]
-            ++ (with epkgs.elpaPackages; [
-              org
-            ])
-          );
-        in
-        {
-          ci = pkgs.mkShell {
-            buildInputs = with pkgs; [ 
-              gnumake
-              customEmacs
+          texenv = pkgs.texlive.combine {
+            inherit (pkgs.texlive)
+              beamer
+              beamertheme-simpleplus
+              collection-basic
+              collection-fontsextra
+              collection-fontsrecommended
+              collection-langenglish
+              collection-langportuguese
+              collection-latex
+              collection-latexextra
+              collection-mathscience
+              enumitem
+              fancyhdr
+              fontawesome
+              graphics
+              graphics-cfg
               graphviz
-              texenv
-            ];
+              hyphen-portuguese
+              latexmk
+              textcase
+              scheme-medium
+            ;
           };
 
-          # `nix develop`
-          default = devenv.lib.mkShell {
-            inherit inputs pkgs;
-            modules = [
-              ({ pkgs, lib, ... }: {
-                packages = with pkgs; [
-                  bash
-                  gnumake
-                  graphviz
-                  texenv
-                ];
-
-                languages.texlive.enable = true;
-              })
-            ];
+          customEmacs = (pkgs.emacsPackagesFor pkgs.emacs-nox).emacsWithPackages (
+            epkgs:
+            (with epkgs.melpaPackages; 
+              [ citeproc htmlize ]
+              ++ (with epkgs.elpaPackages; [ org ])
+            )
+          );
+        in
+        {
+          # This sets `pkgs` to a nixpkgs with allowUnfree option set.
+          _module.args.pkgs = import nixpkgs {
+            inherit system;
+            config.allowUnfree = true;
           };
-        });
+
+          # nix build
+          packages = {
+            default = pkgs.stdenvNoCC.mkDerivation rec {
+              name = "presentation";
+              src = ./.;
+              buildInputs = with pkgs; [ bash coreutils customEmacs gnumake texenv ];
+              phases = ["unpackPhase" "buildPhase" "installPhase"];
+              buildPhase = ''
+                export PATH="${pkgs.lib.makeBinPath buildInputs}:$PATH"
+                export XDG_CACHE_HOME="$(mktemp -d)"
+                export HOME="$(mktemp -d)"
+                export SOURCE_DATE_EPOCH="${toString self.lastModified}"
+                make build
+              '';
+              installPhase = ''
+                mkdir -p $out
+                cp presentation.pdf $out
+              '';
+            };
+          };
+
+          # nix develop
+          devShells = {
+            ci = pkgs.mkShell {
+              buildInputs = with pkgs; [ gnumake customEmacs graphviz texenv ];
+            };
+
+            default = devenv.lib.mkShell {
+              inherit inputs pkgs;
+              modules = [
+                ({ pkgs, lib, ... }: {
+                  packages = with pkgs; [ bash gnumake graphviz texenv ];
+
+                  env = {
+                    SOURCE_DATE_EPOCH="${toString self.lastModified}";
+                  };
+
+                  languages.texlive.enable = true;
+                })
+              ];
+            };
+          };
+        };
+
+        flake = {};
     };
 }
